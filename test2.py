10f75872e5df999c3e4d6600ce30f490e7b623ae
Iiyana <iliyana.kehayova2407@gmail.com>
2025-11-06 13:26:13 +0100

    test 2

---
+++ test2.py
@@ -0,0 +1,16 @@
+print("Hello world")
+
+
+number = 8.63
+
+print(f"the number on the screen is {number}")
+
+number = int(number)
+
+print(number)
+
+age = input("How old are you? ")
+print(f"You are {age} years old")
+age=int(age)
+age=age+1
+print(f"Next year you will be {age} years old")
