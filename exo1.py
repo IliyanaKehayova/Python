2dd884730801618f633c7a3849bec74760cf709c
Iiyana <iliyana.kehayova2407@gmail.com>
2025-11-06 14:11:24 +0100

    exo python

---
+++ exo1.py
@@ -0,0 +1,5 @@
+item = input("What item would you like to buy? ")
+price = float(input("What is the price of this item? "))
+quantity = int(input("How many item would you like to buy? "))
+total = price * quantity
+print(f"Your total is {total} dollars")
