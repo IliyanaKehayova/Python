c88a3b639fc246f92ed20eceb2599958d5d6a283
Iliyana <iliyana.kehayova2407@gmail.com>
2025-11-11 23:08:55 +0100

    exo python

--- exo10.py
+++ exo10.py
@@ -1,3 +1,16 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
+import time
+
+#for count in range(5):
+    #print(time.ctime())
+    # Prints the current time with a five second difference
+    #time.sleep(5)
+
+
+my_time = int(input("Enter number on seconds: "))
+
+for x in range(my_time, 0, -1):
+    print(x)
+    time.sleep(1)
+
+print("Time's up!")
+
