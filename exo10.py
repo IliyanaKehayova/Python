235608ce13376b13a5e48fcf988b0048b6a65358
Iliyana <iliyana.kehayova2407@gmail.com>
2025-11-16 23:51:34 +0100

    exo python

--- exo10.py
+++ exo10.py
@@ -6,10 +6,13 @@ import time
     #time.sleep(5)
 
 
-my_time = int(input("Enter number on seconds: "))
+my_time = int(input("Enter number in seconds: "))
 
 for x in range(my_time, 0, -1):
-    print(x)
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("Time's up!")
